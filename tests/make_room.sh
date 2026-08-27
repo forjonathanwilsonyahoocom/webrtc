cf30606622997b006949cb105f77c5692e3c8716
jonathan.nogpus@mindbodyengineer.com <oem@princesspawn>
2026-08-27 16:53:04 -0600

    makes a room

---
+++ tests/make_room.sh
@@ -0,0 +1,33 @@
+JANUS=http://localhost:8088/janus
+SESSION_ID=$(curl -s -X POST -H "Content-Type: application/json" \
+   -d '{"janus":"create",
+        "transaction":"t-create"
+      }' \
+   $JANUS | jq -r '.data.session_id')
+
+# 2. Attach plugin
+HANDLE_ID=$(curl -s -X POST -H "Content-Type: application/json" \
+   -d '{
+         "janus":"attach",
+	 "plugin":"janus.plugin.videoroom",
+	 "transaction":"t-attach",
+	 "session_id":$SESSION_ID
+       }' \
+   $JANUS | jq -r '.data.handle_id')
+
+# 3. Create a test room (room id = 1234)
+curl -s -X POST "$JANUS/$SESSION_ID/$HANDLE_ID" \
+  -H 'Content-Type: application/json' \
+  -d '{
+      "janus": "create",
+      "transaction": "t-create-room",
+      "room": 1234,
+      "description": "Audio-only test room",
+      "publishers": 10,
+      "is_private": false,
+      "audiocodec": "opus",
+      "videocodec": "none",
+      "audiolevel_ext": true,
+      "audiolevel_event": true
+    }'
+
